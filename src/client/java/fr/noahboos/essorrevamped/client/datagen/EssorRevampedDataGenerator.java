35c472f2ab37908c100db9931239f2d28dc8aaab
Noah Boos <noahboos-dev@proton.me>
2026-08-30 16:44:31 +0200

    Chore - Datagen has been setup.

---
+++ src/client/java/fr/noahboos/essorrevamped/client/datagen/EssorRevampedDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.noahboos.essorrevamped.client.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EssorRevampedDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
