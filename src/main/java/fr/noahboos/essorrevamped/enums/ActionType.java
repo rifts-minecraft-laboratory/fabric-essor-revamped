9bbf642df6c70fc90b943d765ec0872bc3e52e1a
Noah Boos <noahboos-dev@proton.me>
2026-08-30 23:34:20 +0200

    Feature - Added the ActionType enumerator.

---
+++ src/main/java/fr/noahboos/essorrevamped/enums/ActionType.java
@@ -0,0 +1,10 @@
+package fr.noahboos.essorrevamped.enums;
+
+public enum ActionType {
+    // Enumerator values here.
+    ;
+
+    public boolean is(ActionType other) {
+        return this == other;
+    }
+}
