ca3eaa7c6c012bc70e40329e39653d0d2b07d8a8
Noah Boos <noahboos-dev@proton.me>
2026-09-06 23:27:42 +0200

    Feature - Added the player block break event behaviour.

--- src/main/java/fr/noahboos/essorrevamped/events/EssorRevampedEvents.java
+++ src/main/java/fr/noahboos/essorrevamped/events/EssorRevampedEvents.java
@@ -3,10 +3,14 @@ package fr.noahboos.essorrevamped.events;
 import fr.noahboos.essorrevamped.EssorRevamped;
 import fr.noahboos.essorrevamped.components.EssorRevampedComponents;
 import fr.noahboos.essorrevamped.components.definitions.progression.Progression;
+import fr.noahboos.essorrevamped.components.definitions.progression.ProgressionService;
 import fr.noahboos.essorrevamped.enums.ActionType;
+import fr.noahboos.essorrevamped.experiencetables.ExperienceTable;
 import fr.noahboos.essorrevamped.experiencetables.ExperienceTableService;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.item.v1.DefaultItemComponentEvents;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.ItemStack;
 
 public class EssorRevampedEvents {
     public static void initialize() {
@@ -17,7 +21,17 @@ public class EssorRevampedEvents {
             });
         });
         PlayerBlockBreakEvents.AFTER.register((world, player, blockPos, blockState, blockEntity) -> {
-            System.out.println(ExperienceTableService.findExperienceTable(world.getServer().getResourceManager(), player.getActiveItem(), ActionType.MINING));
+            ExperienceTable experienceTable = ExperienceTableService.findExperienceTable(world.getServer().getResourceManager(), player.getActiveItem(), ActionType.MINING);
+            if (experienceTable == null) return;
+            float experienceToGain = experienceTable.values().getOrDefault(BuiltInRegistries.BLOCK.getKey(blockState.getBlock()), 0f);
+
+            ItemStack itemStack = player.getActiveItem();
+            Progression progression = itemStack.get(EssorRevampedComponents.PROGRESSION);
+
+            if (progression == null) return;
+
+            progression = ProgressionService.gainExperiencePoints(progression, experienceToGain);
+            itemStack.set(EssorRevampedComponents.PROGRESSION, progression);
         });
         EssorRevamped.LOGGER.info("Registered {}'s events.", EssorRevamped.MOD_ID);
     }
