2d84f3f1fc1647b927f9d11e82e7680a27e90902
Noah Boos <noahboos-dev@proton.me>
2026-09-08 16:46:30 +0200

    Feature - Added mutation methods to the `ArmorData` record.

--- src/main/java/fr/noahboos/essorrevamped/records/ArmorData.java
+++ src/main/java/fr/noahboos/essorrevamped/records/ArmorData.java
@@ -13,4 +13,34 @@ public record ArmorData(
     public ArmorData(EquipmentSlot equipmentSlot, ItemStack itemStack) {
         this(equipmentSlot, itemStack, 0, 0, 0);
     }
+
+    public ArmorData withArmor(double armor) {
+        return new ArmorData(
+            this.equipmentSlot(),
+            this.itemStack(),
+            armor,
+            this.armorToughness(),
+            this.enchantmentProtectionFactor()
+        );
+    }
+
+    public ArmorData withArmorToughness(double armorToughness) {
+        return new ArmorData(
+            this.equipmentSlot(),
+            this.itemStack(),
+            this.armor(),
+            armorToughness,
+            this.enchantmentProtectionFactor()
+        );
+    }
+
+    public ArmorData withEnchantmentProtectionFactor(int enchantmentProtectionFactor) {
+        return new ArmorData(
+            this.equipmentSlot(),
+            this.itemStack(),
+            this.armor(),
+            this.armorToughness(),
+            enchantmentProtectionFactor
+        );
+    }
 }
