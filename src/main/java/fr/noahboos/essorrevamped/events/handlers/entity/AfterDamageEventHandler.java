d8f4e37b37762533f66ebd0a3b95a46edaa916bc
Noah Boos <noahboos-dev@proton.me>
2026-09-10 19:25:13 +0200

    Refactor - Split the `AfterDamageEventHandler` class in three regions with their core method each.

--- src/main/java/fr/noahboos/essorrevamped/events/handlers/entity/AfterDamageEventHandler.java
+++ src/main/java/fr/noahboos/essorrevamped/events/handlers/entity/AfterDamageEventHandler.java
@@ -9,8 +9,10 @@ import fr.noahboos.essorrevamped.records.EnchantmentProtectionRule;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.enchantment.Enchantment;
@@ -21,51 +23,69 @@ import java.util.Map;
 
 public class AfterDamageEventHandler {
     public static void register() {
-        ServerLivingEntityEvents.AFTER_DAMAGE.register((entity, source, baseDamageTaken, damageTaken, blocked) -> {
-            List<ArmorPieceData> armorPieces = new ArrayList<>();
-            armorPieces.add(new ArmorPieceData(EquipmentSlot.HEAD, entity.getItemBySlot(EquipmentSlot.HEAD)));
-            armorPieces.add(new ArmorPieceData(EquipmentSlot.CHEST, entity.getItemBySlot(EquipmentSlot.CHEST)));
-            armorPieces.add(new ArmorPieceData(EquipmentSlot.LEGS, entity.getItemBySlot(EquipmentSlot.LEGS)));
-            armorPieces.add(new ArmorPieceData(EquipmentSlot.FEET, entity.getItemBySlot(EquipmentSlot.FEET)));
-
-            if (source.is(DamageTypes.DROWN)) return;
-
-            armorPieces.forEach(armorPieceData -> {
-                EssorRevamped.LOGGER.info("Gathering data from {}.", armorPieceData.itemStack().getItemName().getString());
-
-                ItemAttributeModifiers itemAttributeModifiers = armorPieceData.itemStack().get(DataComponents.ATTRIBUTE_MODIFIERS);
-                if (itemAttributeModifiers == null) {
-                    EssorRevamped.LOGGER.warn("Can't gather data from {} as it has no attribute modifier data component.", armorPieceData.itemStack().getItemName().getString());
-                    return;
-                }
-
-                ArmorPieceData _armorPieceData = armorPieceData
-                    .withArmor(AttributeHelper.getAttributeValue(itemAttributeModifiers, Attributes.ARMOR))
-                    .withArmorToughness(AttributeHelper.getAttributeValue(itemAttributeModifiers, Attributes.ARMOR_TOUGHNESS));
-
-                for (EnchantmentProtectionRule enchantmentProtectionRule : EnchantmentHelper.enchantmentProtectionRules) {
-                    if (!enchantmentProtectionRule.appliesTo().test(source)) continue;
-
-                    Map.Entry<Holder<Enchantment>, Integer> enchantment = EnchantmentHelper.getEnchantment(_armorPieceData.itemStack().getEnchantments(), enchantmentProtectionRule.enchantment());
-
-                    if (enchantment == null) continue;
-
-                    _armorPieceData = _armorPieceData.withEnchantmentProtectionFactor(_armorPieceData.enchantmentProtectionFactor() + (enchantmentProtectionRule.baseEnchantmentProtectionFactor() * enchantment.getValue()));
-                };
-
-                EssorRevamped.LOGGER.info("Gathered data from {}.", _armorPieceData.itemStack().getItemName().getString());
-                EssorRevamped.LOGGER.info("Rewarding {} with experience.", _armorPieceData.itemStack().getItemName().getString());
-
-                if (source.is(DamageTypes.FALL)) {
-                    if (_armorPieceData.equipmentSlot() != EquipmentSlot.FEET) return;
-                }
-
-                float experienceToGain = (float) (damageTaken * (1f + (_armorPieceData.armor() * 0.05f) + (_armorPieceData.armorToughness() * 0.10f) + (_armorPieceData.enchantmentProtectionFactor() * 0.10f))) * 2.0f;
-
-                ProgressionService.progressItem(_armorPieceData.itemStack(), experienceToGain);
-
-                EssorRevamped.LOGGER.info("Rewarded {} with experience.", _armorPieceData.itemStack().getItemName().getString());
-            });
+        ServerLivingEntityEvents.AFTER_DAMAGE.register((entity, damageSource, baseDamageTaken, damageTaken, blocked) -> {
+            AfterDamageEventHandler.handleArmor(entity, damageSource, damageTaken);
         });
     }
+
+    // <editor-fold desc="Region - Hurt entity's armor handling after damage has been taken." defaultstate="collapsed">
+    public static void handleArmor(LivingEntity entity, DamageSource source, float damageTaken) {
+        List<ArmorPieceData> armorPieces = new ArrayList<>();
+        armorPieces.add(new ArmorPieceData(EquipmentSlot.HEAD, entity.getItemBySlot(EquipmentSlot.HEAD)));
+        armorPieces.add(new ArmorPieceData(EquipmentSlot.CHEST, entity.getItemBySlot(EquipmentSlot.CHEST)));
+        armorPieces.add(new ArmorPieceData(EquipmentSlot.LEGS, entity.getItemBySlot(EquipmentSlot.LEGS)));
+        armorPieces.add(new ArmorPieceData(EquipmentSlot.FEET, entity.getItemBySlot(EquipmentSlot.FEET)));
+
+        if (source.is(DamageTypes.DROWN)) return;
+
+        armorPieces.forEach(armorPieceData -> {
+            EssorRevamped.LOGGER.info("Gathering data from {}.", armorPieceData.itemStack().getItemName().getString());
+
+            ItemAttributeModifiers itemAttributeModifiers = armorPieceData.itemStack().get(DataComponents.ATTRIBUTE_MODIFIERS);
+            if (itemAttributeModifiers == null) {
+                EssorRevamped.LOGGER.warn("Can't gather data from {} as it has no attribute modifier data component.", armorPieceData.itemStack().getItemName().getString());
+                return;
+            }
+
+            ArmorPieceData _armorPieceData = armorPieceData
+                .withArmor(AttributeHelper.getAttributeValue(itemAttributeModifiers, Attributes.ARMOR))
+                .withArmorToughness(AttributeHelper.getAttributeValue(itemAttributeModifiers, Attributes.ARMOR_TOUGHNESS));
+
+            for (EnchantmentProtectionRule enchantmentProtectionRule : EnchantmentHelper.enchantmentProtectionRules) {
+                if (!enchantmentProtectionRule.appliesTo().test(source)) continue;
+
+                Map.Entry<Holder<Enchantment>, Integer> enchantment = EnchantmentHelper.getEnchantment(_armorPieceData.itemStack().getEnchantments(), enchantmentProtectionRule.enchantment());
+
+                if (enchantment == null) continue;
+
+                _armorPieceData = _armorPieceData.withEnchantmentProtectionFactor(_armorPieceData.enchantmentProtectionFactor() + (enchantmentProtectionRule.baseEnchantmentProtectionFactor() * enchantment.getValue()));
+            };
+
+            EssorRevamped.LOGGER.info("Gathered data from {}.", _armorPieceData.itemStack().getItemName().getString());
+            EssorRevamped.LOGGER.info("Rewarding {} with experience.", _armorPieceData.itemStack().getItemName().getString());
+
+            if (source.is(DamageTypes.FALL)) {
+                if (_armorPieceData.equipmentSlot() != EquipmentSlot.FEET) return;
+            }
+
+            float experienceToGain = (float) (damageTaken * (1f + (_armorPieceData.armor() * 0.05f) + (_armorPieceData.armorToughness() * 0.10f) + (_armorPieceData.enchantmentProtectionFactor() * 0.10f))) * 2.0f;
+
+            ProgressionService.progressItem(_armorPieceData.itemStack(), experienceToGain);
+
+            EssorRevamped.LOGGER.info("Rewarded {} with experience.", _armorPieceData.itemStack().getItemName().getString());
+        });
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="Region - Hurt entity's shield handling after damage has been blocked." defaultstate="collapsed">
+    public static void handleShield() {
+        // To implement.
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="Region - Damage source's weapon handling after damage has been taken." defaultstate="collapsed">
+    public static void handleWeapon() {
+        // To implement.
+    }
+    // </editor-fold>
 }
