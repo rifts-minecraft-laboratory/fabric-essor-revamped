e2b16d2f45f40ee6799f3f8c128a0ff7cc5c7679
Noah Boos <noahboos-dev@proton.me>
2026-09-11 20:13:22 +0200

    Feature - Added the `ENTITY_KILLING` `Action Type`.

--- src/main/java/fr/noahboos/essorrevamped/enums/ActionType.java
+++ src/main/java/fr/noahboos/essorrevamped/enums/ActionType.java
@@ -1,7 +1,8 @@
 package fr.noahboos.essorrevamped.enums;
 
 public enum ActionType {
-    BLOCK_BREAKING;
+    BLOCK_BREAKING,
+    ENTITY_KILLING;
 
     public boolean is(ActionType other) {
         return this == other;
