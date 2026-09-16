dd0ae7c3bb51dd61b066c542c931a9a8470d4ad3
Noah Boos <noahboos-dev@proton.me>
2026-09-16 21:12:15 +0200

    Feature - Implemented the experience reward for breaking blocks with a pickaxe.

--- src/main/java/fr/noahboos/essorrevamped/experiencetables/EssorRevampedExperienceTableProvider.java
+++ src/main/java/fr/noahboos/essorrevamped/experiencetables/EssorRevampedExperienceTableProvider.java
@@ -8,6 +8,7 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.EntityTypes;
+import net.minecraft.world.level.block.Blocks;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -29,7 +30,44 @@ public class EssorRevampedExperienceTableProvider extends FabricCodecDataProvide
         )));
 
         provider.accept(Identifier.fromNamespaceAndPath(EssorRevamped.MOD_ID, "block-breaking-pickaxe"), new ExperienceTable(Map.ofEntries(
-            //
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.STONE), 1.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.GRANITE), 1.25f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.DIORITE), 1.25f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.ANDESITE), 1.25f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.SANDSTONE), 1.75f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.RED_SANDSTONE), 1.75f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.TUFF), 1.5f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.CINNABAR), 2.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.SULFUR), 2.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.SMOOTH_BASALT), 1.75f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.CALCITE), 1.75f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.AMETHYST_BLOCK), 1.75f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.BUDDING_AMETHYST), 1.75f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.DRIPSTONE_BLOCK), 1.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.COPPER_ORE), 1.25f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.COAL_ORE), 1.5f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.IRON_ORE), 1.75f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.GOLD_ORE), 2.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.DIAMOND_ORE), 2.5f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.EMERALD_ORE), 3.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.DEEPSLATE), 1.5f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.DEEPSLATE_COPPER_ORE), 1.875f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.DEEPSLATE_COAL_ORE), 2.25f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.DEEPSLATE_IRON_ORE), 2.625f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.DEEPSLATE_GOLD_ORE), 3.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.DEEPSLATE_DIAMOND_ORE), 3.75f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.DEEPSLATE_EMERALD_ORE), 4.5f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.OBSIDIAN), 3.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.CRYING_OBSIDIAN), 3.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.NETHERRACK), 0.125f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.CRIMSON_NYLIUM), 0.125f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.WARPED_NYLIUM), 0.125f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.BASALT), 2.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.BLACKSTONE), 2.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.NETHER_GOLD_ORE), 3.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.NETHER_QUARTZ_ORE), 2.625f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.ANCIENT_DEBRIS), 8.0f),
+            Map.entry(BuiltInRegistries.BLOCK.getKey(Blocks.END_STONE), 1.5f)
         )));
 
         provider.accept(Identifier.fromNamespaceAndPath(EssorRevamped.MOD_ID, "block-breaking-shears"), new ExperienceTable(Map.ofEntries(
